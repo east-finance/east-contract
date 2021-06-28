f584e8b7faf6fc6047a0a295385fe076f6544cf6
mtokarev <mtokarev@web3tech.ru>
2021-06-28 13:51:47 +0300

    jest tests

---
+++ src/tests/config.ts
@@ -0,0 +1,25 @@
+import * as path from 'path'
+import { config, DotenvConfigOutput } from "dotenv"
+
+const REQUIRED_ENVS = {
+  NODE_ADDRESS: '',
+  SEED_PHRASE: '',
+}
+
+const envs = config({ path: path.resolve(process.cwd(), '.env.test') }) as Omit<DotenvConfigOutput, 'parsed'> & { parsed: typeof REQUIRED_ENVS }
+
+function validateEnvs() {
+  const missingVariables = []
+  const parsedEnvs = Object.keys(envs.parsed)
+  for (const requiredEnv of Object.keys(REQUIRED_ENVS)) {
+    if (!parsedEnvs.includes(requiredEnv)) {
+      missingVariables.push(requiredEnv)
+    }
+  }
+  if (missingVariables.length > 0) {
+    throw new Error(`The following env variables are missing: ${missingVariables.join(', ')}`)
+  }
+}
+validateEnvs()
+
+export const { NODE_ADDRESS, SEED_PHRASE } = envs.parsed
---
+++ src/tests/utils.ts
@@ -0,0 +1,41 @@
+import { create, MAINNET_CONFIG, WeSdk } from '@wavesenterprise/js-sdk';
+import nodeFetch from 'node-fetch';
+import { NODE_ADDRESS, SEED_PHRASE } from './config';
+
+export interface Globals {
+  fetch?: typeof fetch,
+  weSdk?: WeSdk,
+  keyPair?: { publicKey: string, privateKey: string },
+  address?: string,
+}
+
+export async function initGlobals(): Promise<Required<Globals>> {
+  const globals: Globals = {}
+  globals.fetch = (url: RequestInfo, options?: RequestInit): Promise<Response> => {
+    // @ts-ignore
+    return nodeFetch(url, {
+      ...options,
+      headers: {
+        ...options?.headers,
+        'x-api-key': 'we',
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+  const { chainId, minimumFee } = await(await globals.fetch(`${NODE_ADDRESS}/node/config`)).json()
+  const wavesApiConfig = {
+    ...MAINNET_CONFIG,
+    nodeAddress: NODE_ADDRESS,
+    crypto: 'waves',
+    networkByte: chainId.charCodeAt(0),
+    minimumFee
+  };
+  globals.weSdk = create({
+    initialConfiguration: wavesApiConfig,
+    fetchInstance: globals.fetch,
+  });
+  const seed = globals.weSdk.Seed.fromExistingPhrase(SEED_PHRASE)
+  globals.address = seed.address
+  globals.keyPair = seed.keyPair
+  return globals as Required<Globals>
+}
