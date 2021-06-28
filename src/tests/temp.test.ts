8a3026fc4d8278796f6c44383bb99aa6231f01fa
mtokarev <mtokarev@web3tech.ru>
2021-06-28 21:04:50 +0300

    jest tests

--- src/tests/temp.test.ts
+++ src/tests/temp.test.ts
@@ -1,6 +1,6 @@
 import { plainToClass } from "class-transformer"
 import { validate } from "class-validator"
-import { ConfigDto } from "../data-transfer-objects"
+import { ConfigDto } from "../dto/config.dto"
 import { Globals, initGlobals } from "./utils"
 
 let globals: Required<Globals>
@@ -10,13 +10,12 @@ beforeAll(async () => {
 })
 
 test('config dto validation', async () => {
-  // const { rpcService } = globals
-  // rpcService.handleDockerCreate()
-  const configDto: ConfigDto = {
+  const { rpcService } = globals
+  const invalidConfig: ConfigDto = {
     oracleContractId: 'oracleContractId',
     oracleTimestampMaxDiff: -1,
     // @ts-ignore
-    ç:  '0.5',
+    rwaPart: '0.5',
     westCollateral: 2.5,
     liquidationCollateral: 1.3,
     minHoldTime: 1000 * 60 * 60,
@@ -24,7 +23,27 @@ test('config dto validation', async () => {
     // @ts-ignore
     issueEnabled: 'yes',
   }
-  const errors = await validate(plainToClass(ConfigDto, configDto))
-  expect(errors.length).toBe(3)
-  expect(errors.map(error => error.property).sort()).toEqual(['oracleTimestampMaxDiff', 'rwaPart', 'issueEnabled'].sort())
+  expect(
+    await rpcService.handleDockerCreate({
+      id: '',
+      type: 103,
+      sender: 'sender',
+      sender_public_key: 'pk',
+      contract_id: 'contract_id',
+      fee: 1000,
+      version: 1,
+      proofs: Buffer.from(''),
+      timestamp: Date.now(),
+      fee_asset_id: {
+        value: 'fee_asset_id',
+      },
+      params: [
+        {
+          key: 'config',
+          value: 'string_value',
+          string_value: '' // JSON.stringify(invalidConfig)
+        }
+      ],
+    })  
+  ).rejects.toThrowError(Error)
 })
