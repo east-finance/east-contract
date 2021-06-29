30dc0aa256fe22c16842fd82c749577c100075ae
mtokarev <mtokarev@web3tech.ru>
2021-06-29 17:44:53 +0300

    jest tests

--- src/tests/mocks.ts
+++
@@ -1,6 +0,0 @@
-import { DataEntryRequest } from "../interfaces";
-import { StateService } from "../services/StateService";
-
-export const StateServiceMock: StateService = {
-  commitSuccess(txId: string, results: DataEntryRequest[]) {}
-} as StateService
