2e32249f96024458ce27e7039767436584852330
mtokarev <mtokarev@web3tech.ru>
2021-08-02 12:06:38 +0300

    tests

--- src/tests/utils/east-service-api/get-tx-statuses.ts
+++ src/tests/utils/east-service-api/get-tx-statuses.ts
@@ -2,14 +2,14 @@ import nodeFetch from 'node-fetch'
 import { EAST_SERVICE_ADDRESS } from '../../config';
 
 export async function getTxStatuses(fetch: typeof nodeFetch, address: string, limit: number, offset: number) {
-  const { status, json } = await fetch(`${EAST_SERVICE_ADDRESS}/v1/user/transactions/statuses?address=${address}&limit=${limit}`, {
+  const data = await fetch(`${EAST_SERVICE_ADDRESS}/v1/user/transactions/statuses?address=${address}&limit=${limit}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
   });
-  if (status !== 200) {
+  if (data.status !== 200) {
     throw new Error('Failed to fetch.')
   }
-  return json()
+  return data.json()
 }
--- src/tests/utils/east-service-api/track-tx.ts
+++ src/tests/utils/east-service-api/track-tx.ts
@@ -9,15 +9,15 @@ export interface TrackTxRequest {
 }
 
 export async function trackTx(fetch: typeof nodeFetch, request: TrackTxRequest) {
-  const { status, text } = await fetch(`${EAST_SERVICE_ADDRESS}/v1/user/transactions/statuses`, {
+  const data = await fetch(`${EAST_SERVICE_ADDRESS}/v1/user/transactions/statuses`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(request)
   });
-  if (status !== 201) {
+  if (data.status !== 201) {
     throw new Error('Failed to fetch.')
   }
-  return
+  return data.json()
 }
