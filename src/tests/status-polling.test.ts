0f4d5bc6a7df6646babebf03efba83c1b9d597e5
mtokarev <mtokarev@web3tech.ru>
2021-08-02 10:55:42 +0300

    tests

--- src/tests/status-polling.test.ts
+++ src/tests/status-polling.test.ts
@@ -1,18 +1,33 @@
 import { readFileSync } from "fs";
 import { PATH_TO_USER_SEEDS } from "./config";
+import { TxTypes } from "./constants";
 import { initGlobals } from "./utils";
-import { Globals } from "./utils/interfaces";
+import { runPolling } from "./utils/polling";
 
-let globals: Required<Globals>
+async function main() {
+  try {
+    const { weSdk, contractApi, eastServiceApi } = await initGlobals();
+    const result = readFileSync(PATH_TO_USER_SEEDS!)
+    const parsedResult = JSON.parse(result.toString())
+    const userSeed = weSdk.Seed.fromExistingPhrase(parsedResult.seeds[0])
+    const mintTxId = await contractApi.mint(userSeed, 4)
+    eastServiceApi.trackTx({
+      address: userSeed.address,
+      txId: mintTxId,
+      type: TxTypes.mint,
+    })
+    runPolling({
+      sourceFn: () => eastServiceApi.getTxStatuses(userSeed.address, 100, 0),
+      predicateFn: (result: any) => {
+        console.log(result)
+        return false
+      },
+      pollInterval: 1000,
+      timeout: 15000,
+    })
+  } catch (err) {
+    console.log(err.message)
+  }
+}
 
-beforeAll(async () => {
-  globals = await initGlobals();
-})
-
-test('mint', async () => {
-  const { weSdk, contractApi, eastServiceApi } = globals
-  const result = readFileSync(PATH_TO_USER_SEEDS!)
-  const parsedResult = JSON.parse(result.toString())
-  const userSeed = weSdk.Seed.fromExistingPhrase(parsedResult.seeds[0])
-  console.log(await contractApi.mint(userSeed, 10))
-})
+main()
