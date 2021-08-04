616cd2f9e4b3f03d99b1d1c70072bcd5b91352a2
mtokarev <mtokarev@web3tech.ru>
2021-08-04 18:51:15 +0300

    recalculate vault bugfix

--- src/services/RPCService.ts
+++ src/services/RPCService.ts
@@ -280,8 +280,9 @@ export class RPCService {
     const rwaPartInPosition = rwaPart / ((1 - rwaPart) * westCollateral + rwaPart)
     const usdToRwaAmount = rwaPartInPosition * usdTotal
     const rwaAmount = usdToRwaAmount / rwaRate.value
-    const eastAmount = rwaPart > 0 ? roundValue(rwaAmount / rwaPart) : 0
+    const eastPriceInWest = (rwaPart / westRate.value) + ((1 - rwaPart) / westRate.value * westCollateral)
     const westAmount = (usdTotal - usdToRwaAmount) / westRate.value
+    const eastAmount = westAmount / eastPriceInWest
 
     if (eastAmount > oldEastAmount) {
       return {
