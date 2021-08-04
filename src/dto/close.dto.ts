7474440bfe3e6470e0c428471c710439f964ee32
mtokarev <mtokarev@web3tech.ru>
2021-08-04 17:16:16 +0300

    bugfix

--- src/dto/close.dto.ts
+++ src/dto/close.dto.ts
@@ -1,13 +1,15 @@
-import { IsString } from "class-validator";
+import { IsString, IsOptional } from "class-validator";
 import { CloseParam } from "../interfaces";
 
 export class CloseDto implements CloseParam {
   @IsString()
   address!: string;
   
+  @IsOptional()
   @IsString()
   westTransferId!: string;
   
+  @IsOptional()
   @IsString()
   rwaTransferId!: string;
 }
