87cd67ae2037ae98e01e631ec4f4752e30d4aa42
mtokarev <mtokarev@web3tech.ru>
2021-08-05 20:26:10 +0300

    tests

--- src/tests/scripts/update-oracle-contract-rates.ts
+++ src/tests/scripts/update-oracle-contract-rates.ts
@@ -11,34 +11,34 @@ async function main(west?: number, rwa?: number) {
   if (rwa !== undefined) {
     txIds.push(await oracleContractApi.updateRates({ key: 'rwa', value: rwa }))
   }
-  const pollingPromises: Promise<GetTxStatusResponse | PollingTimeoutError>[] = []
-  txIds.forEach(txId => {
-    pollingPromises.push(
-      runPolling<GetTxStatusResponse>({
-        sourceFn: async () => {
-          try {
-            return await nodeApi.getTxStatus(txId)
-          } catch (err) {
-            if (err instanceof GetTxStatusError) {
-              return err
+  console.log(
+    await Promise.all(
+      txIds.map(txId => {
+        return runPolling<GetTxStatusResponse>({
+          sourceFn: async () => {
+            try {
+              return await nodeApi.getTxStatus(txId)
+            } catch (err) {
+              if (err instanceof GetTxStatusError) {
+                return err
+              }
             }
-          }
-        },
-        predicateFn: (result: GetTxStatusResponse | GetTxStatusError | undefined) => {
-          if (result === undefined) {
-            return false
-          }
-          if (result instanceof GetTxStatusError) {
-            return false
-          }
-          return result.every(nodeResponse => nodeResponse.status === 'Success')
-        },
-        pollInterval: 1000,
-        timeout: 60000,
+          },
+          predicateFn: (result: GetTxStatusResponse | GetTxStatusError | undefined) => {
+            if (result === undefined) {
+              return false
+            }
+            if (result instanceof GetTxStatusError) {
+              return false
+            }
+            return result.every(nodeResponse => nodeResponse.status === 'Success')
+          },
+          pollInterval: 1000,
+          timeout: 60000,
+        })
       })
     )
-  })
-  console.log(await Promise.all(pollingPromises))
+  )
 }
 
 const west: number | undefined = process.env.WEST === undefined ? undefined : parseFloat(process.env.WEST)
