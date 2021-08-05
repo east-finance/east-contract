c0a1da1b410a2541f40fcfa48aa2eb61eb588382
mtokarev <mtokarev@web3tech.ru>
2021-08-05 20:24:11 +0300

    tests

--- src/tests/scripts/update-oracle-contract-rates.ts
+++ src/tests/scripts/update-oracle-contract-rates.ts
@@ -1,11 +1,48 @@
-import { readFileSync } from "fs";
-import { NODE_ADDRESS, PATH_TO_USER_SEEDS } from "../config";
 import { initGlobals } from "../utils";
-import { runPolling } from "../utils/polling";
+import { GetTxStatusError, GetTxStatusResponse } from "../utils/node-api/get-tx-status";
+import { PollingTimeoutError, runPolling } from "../utils/polling";
 
-async function main() {
-  const { weSdk, minimumFee, seed: ownerSeed, fetch } = await initGlobals()
-  
+async function main(west?: number, rwa?: number) {
+  const { oracleContractApi, nodeApi } = await initGlobals()
+  const txIds = []
+  if (west !== undefined) {
+    txIds.push(await oracleContractApi.updateRates({ key: 'west', value: west }))
+  }
+  if (rwa !== undefined) {
+    txIds.push(await oracleContractApi.updateRates({ key: 'rwa', value: rwa }))
+  }
+  const pollingPromises: Promise<GetTxStatusResponse | PollingTimeoutError>[] = []
+  txIds.forEach(txId => {
+    pollingPromises.push(
+      runPolling<GetTxStatusResponse>({
+        sourceFn: async () => {
+          try {
+            return await nodeApi.getTxStatus(txId)
+          } catch (err) {
+            if (err instanceof GetTxStatusError) {
+              return err
+            }
+          }
+        },
+        predicateFn: (result: GetTxStatusResponse | GetTxStatusError | undefined) => {
+          if (result === undefined) {
+            return false
+          }
+          if (result instanceof GetTxStatusError) {
+            return false
+          }
+          return result.every(nodeResponse => nodeResponse.status === 'Success')
+        },
+        pollInterval: 1000,
+        timeout: 60000,
+      })
+    )
+  })
+  console.log(await Promise.all(pollingPromises))
 }
 
-main()
+const west: number | undefined = process.env.WEST === undefined ? undefined : parseFloat(process.env.WEST)
+
+const rwa: number | undefined = process.env.RWA === undefined ? undefined : parseFloat(process.env.RWA)
+
+main(west, rwa)
