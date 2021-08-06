f05937480c8d319dbe0ced83a61c43c604bc09ab
mtokarev <mtokarev@web3tech.ru>
2021-08-06 18:14:59 +0300

    tests

--- src/tests/scripts/create-users.ts
+++ src/tests/scripts/create-users.ts
@@ -1,5 +1,5 @@
 import { writeFileSync } from "fs";
-import { NODE_ADDRESS, PATH_TO_USER_SEEDS } from "../config";
+import { NODE_ADDRESS, PATH_TO_USER_SEEDS, RWA_TOKEN_ID } from "../config";
 import { initGlobals } from "../utils";
 import { PollingTimeoutError, runPolling } from "../utils/polling";
 
@@ -22,7 +22,7 @@ function generateUserSeedPhrase() {
   return result.slice(0, -1)
 }
 
-async function main(count: number = 1, westAmount = 3) {
+async function main(count: number = 1, westAmount = 3, rwaAmount = 1) {
   const { weSdk, minimumFee, seed: ownerSeed, fetch } = await initGlobals()
   let pollingResults = []
   const result: any = {
@@ -32,7 +32,7 @@ async function main(count: number = 1, westAmount = 3) {
     const seedPhrase = generateUserSeedPhrase()
     result.seeds.push(seedPhrase)
     const seed = weSdk.Seed.fromExistingPhrase(seedPhrase)
-    const transferCall = weSdk.API.Transactions.Transfer.V3({
+    const westTransferCall = weSdk.API.Transactions.Transfer.V3({
       recipient: seed.address,
       assetId: '',
       amount: westAmount * 100000000,
@@ -41,21 +41,46 @@ async function main(count: number = 1, westAmount = 3) {
       fee: minimumFee[4],
       senderPublicKey: ownerSeed.keyPair.publicKey,
     });
-    const txId = await transferCall.getId(ownerSeed.keyPair.publicKey)
+    const westTransferTxId = await westTransferCall.getId(ownerSeed.keyPair.publicKey)
     pollingResults.push(
       runPolling({
         sourceFn: async () => {
-          const data = await fetch(`${NODE_ADDRESS}/transactions/info/${txId}`)
+          const data = await fetch(`${NODE_ADDRESS}/transactions/info/${westTransferTxId}`)
           return data.json()
         },
         predicateFn: (result: any) => {
-          return result.id !== undefined && result.id === txId
+          return result.id !== undefined && result.id === westTransferTxId
         },
         pollInterval: 1000,
         timeout: 30000,
       })
     )
-    transferCall.broadcast(ownerSeed.keyPair)
+    westTransferCall.broadcast(ownerSeed.keyPair)
+
+    const rwaTransferCall = weSdk.API.Transactions.Transfer.V3({
+      recipient: seed.address,
+      assetId: RWA_TOKEN_ID,
+      amount: westAmount * 100000000,
+      timestamp: Date.now(),
+      attachment: '',
+      fee: minimumFee[4],
+      senderPublicKey: ownerSeed.keyPair.publicKey,
+    });
+    const rwaTransferTxId = await rwaTransferCall.getId(ownerSeed.keyPair.publicKey)
+    pollingResults.push(
+      runPolling({
+        sourceFn: async () => {
+          const data = await fetch(`${NODE_ADDRESS}/transactions/info/${rwaTransferTxId}`)
+          return data.json()
+        },
+        predicateFn: (result: any) => {
+          return result.id !== undefined && result.id === rwaTransferTxId
+        },
+        pollInterval: 1000,
+        timeout: 30000,
+      })
+    )
+    rwaTransferCall.broadcast(ownerSeed.keyPair)
   }
   pollingResults = await Promise.all(pollingResults)
   console.log(pollingResults)
@@ -66,4 +91,6 @@ const count: number | undefined = process.env.COUNT === undefined ? undefined : 
 
 const westAmount: number | undefined = process.env.WEST === undefined ? undefined : parseFloat(process.env.WEST)
 
-main(count, westAmount)
+const rwaAmount: number | undefined = process.env.RWA === undefined ? undefined : parseFloat(process.env.RWA)
+
+main(count, westAmount, rwaAmount)
