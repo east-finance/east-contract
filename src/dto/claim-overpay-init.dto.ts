16ae2cdea7ec197d4cbdc947f928d2068ccbba56
akolodko <temakolodko@gmail.com>
2021-09-25 16:16:49 +0300

    Change claimOverpayInit amount check to optional string

--- src/dto/claim-overpay-init.dto.ts
+++ src/dto/claim-overpay-init.dto.ts
@@ -1,17 +1,7 @@
-import { IsNumber, IsPositive, IsOptional } from "class-validator";
+import { IsString, IsOptional } from "class-validator";
 
 export class ClaimOverpayInitDto {
   @IsOptional()
-  @IsNumber(
-    {
-      allowInfinity: false,
-      allowNaN: false,
-      maxDecimalPlaces: 8,
-    },
-    {
-      message: '$property must be a number (decimal) with no more than 8 decimal places'
-    }
-  )
-  @IsPositive()
-  amount!: number
+  @IsString()
+  amount!: string
 }
