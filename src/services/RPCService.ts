93e7b0fa4865da6bf924835b336ef22707da5db5
akolodko <temakolodko@gmail.com>
2021-10-04 21:08:20 +0300

    Check vault is blocked on transfer

--- src/services/RPCService.ts
+++ src/services/RPCService.ts
@@ -887,6 +887,7 @@ export class RPCService {
         await this.checkIsContractEnabled();
       }
       if ([
+        Operations.transfer,
         Operations.reissue,
         Operations.close_init,
         Operations.claim_overpay_init,
